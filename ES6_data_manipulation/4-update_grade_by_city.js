fa4e3234caeacda2c8c709109981607a71e24f85
MuhammedSultanov <9073@holbertonstudents.com>
2024-07-24 14:03:38 +0400

    map

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrade) {
-  return students.filter((students) => students.location === city)
+    return students.filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = newGrade
         .find((grade) => grade.studentId === student.id);
       return {
-        ...students,
+        ...student,
         grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
