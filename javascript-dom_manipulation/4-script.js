48a3764468d8c59ac4d5c9294fb3e44a3698a8a5
MuhammedSultanov <9073@holbertonstudents.com>
2024-07-28 22:12:22 +0400

    List of elements

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.getElementById("add_item").addEventListener("click", function()
+{
+    var newItem = document.createElement("li");
+    newItem.textContent = "Item";
+    document.querySelector("ul.my_list").appendChild(newItem);
+});
