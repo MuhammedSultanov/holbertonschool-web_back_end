97b314a4fb5ea09a86dee244bb2696064074f852
MuhammedSultanov <9073@holbertonstudents.com>
2024-08-18 14:09:54 +0400

    auto

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Random and asyncio modules are imported
+"""
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    n -- int 
+    max_delay -- int 
+    Return: the list of delay time with asceding order 
+    """
+    new_list = []
+    for i in range(n):
+        a = await wait_random(max_delay)
+        new_list.append(a)
+    for i in range(len(new_list)):
+        for j in range(len(new_list) - 1 - i):
+            if new_list[j] > new_list[j + 1]:
+                t = new_list[j]
+                new_list[j] = new_list[j + 1]
+                new_list[j + 1] = t
+    return new_list
